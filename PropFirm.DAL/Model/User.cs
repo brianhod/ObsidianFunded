b6b99326c46c6c0d7d2fa714abec6f249bf31987
Brian Hodgson <Brian.Hodgson@cstx.de>
2026-02-26 07:03:37 +0100

    Added UserService

--- PropFirm.DAL/Model/User.cs
+++ PropFirm.DAL/Model/User.cs
@@ -10,6 +10,11 @@ namespace PropFirm.Infrastructure.Model
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 
     //[Table("user_configuration")]
---
+++ PropFirm.DAL/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropFirm.Infrastructure.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _dbContext;
+
+        private readonly ILogger<UserService> _logger;
+
+        public UserService(AppDbContext dbContext,
+           ILogger<UserService> logger)
+        {
+
+            _dbContext = dbContext;
+            _logger = logger;
+
+        }
+
+        public string Login(string username, string password)
+        {
+            _dbContext.Users.Where(x => x.UserName == username && x.Password == password);
+
+            return "";
+        }
+    }
+
+}
